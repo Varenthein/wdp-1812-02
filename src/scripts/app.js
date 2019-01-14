20488de28f37f8beb3051930f9600b3436fafca1
Maciej Derewiański <eco_derma@poczta.onet.pl>
2019-01-14 19:10:35 +0100

    Add Tiny-slider to New Furniture section

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,27 @@
 console.log('hello');
+
+var slider = tns({
+    container: '.my-slider',
+    items: 4,
+    slideBy: 'page',
+    controlsContainer: '.controls',
+    mouseDrag: true,
+    gutter: 10,
+    responsive: {
+      320: {
+        items: 1
+      },
+      576: {
+        items: 2
+      },
+      768: {
+        items: 2
+      },
+      992: {
+        items: 3
+      },
+      1200: {
+        items: 4
+      }
+    }
+  });
