89ca34f8428f1d9023d63bab76656678a6ee5f73
Maciej Derewiański <eco_derma@poczta.onet.pl>
2019-01-18 12:54:24 +0100

    Fix indentation

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,25 +1,25 @@
 var slider = tns({
-    container: '.my-slider',
-    items: 4,
-    slideBy: 'page',
-    controlsContainer: '.controls',
-    mouseDrag: true,
-    gutter: 10,
-    responsive: {
-      320: {
-        items: 1
-      },
-      576: {
-        items: 2
-      },
-      768: {
-        items: 2
-      },
-      992: {
-        items: 3
-      },
-      1200: {
-        items: 4
-      }
+  container: '.my-slider',
+  items: 4,
+  slideBy: 'page',
+  controlsContainer: '.controls',
+  mouseDrag: true,
+  gutter: 10,
+  responsive: {
+    320: {
+      items: 1
+    },
+    576: {
+      items: 2
+    },
+    768: {
+      items: 2
+    },
+    992: {
+      items: 3
+    },
+    1200: {
+      items: 4
     }
-  });
+  }
+});
